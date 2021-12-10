70c2791e65ca5b659fc287bc7a2e0804a83963fd
tocuille <thomas.ocuilleanain@ulb.be>
2021-12-10 23:38:09 +0100

    modified common.h

--- src/common.h
+++ src/common.h
@@ -6,67 +6,73 @@
 #include <unistd.h>
 
 int _checked(int ret, const char *calling_function) {
-	if (ret < 0)
-	{
-		perror(calling_function);
-		exit(EXIT_FAILURE);
-	}
-	return ret;
+	// If the function returns less that 0
+    if (ret < 0) {
+        perror(calling_function);
+        exit(EXIT_FAILURE);
+    }
+    return ret;
 }
 
 // The macro allows us to retrieve the name of the calling function
 #define checked(call) _checked(call, #call)
 
 /**
- * @brief Send data under the form <size_t len><...data>
+ * @brief Send data under the form <size_t len><time_t rawtime><...data>
  * Function name is 'ssend' instead of 'send' because the latter already exists.
  */
 void ssend(int sock, void *data, size_t len, time_t rawtime) {
-
-	// Sending message length
-	checked(write(sock, &len, sizeof(len)));
-	// Sending rawtime
-	checked(write(sock, &rawtime, sizeof(rawtime)));
-	// Sending message
-	checked(write(sock, data, len));
+    // Sending message length
+    checked(write(sock, &len, sizeof(len)));
+    // Sending rawtime
+    checked(write(sock, &rawtime, sizeof(rawtime)));
+    // Sending message
+    checked(write(sock, data, len));
 }
 
 /**
  * @brief Receive data under the form <size_t len><data...>.
  */
 size_t receive(int sock, void **message, time_t *now) {
-	
+    
+	// Checking how many bytes last read will receive
 	size_t nbytes_to_receive;
+    if (checked(read(sock, &nbytes_to_receive, sizeof(nbytes_to_receive))) ==
+        0) {
+        // Connection closed
+        return 0;
+    };
 
-	if (checked(read(sock, &nbytes_to_receive, sizeof(nbytes_to_receive))) == 0) {
-		// Connection closed
-		return 0;
-	};
+	// Storing time 
+    time_t rawtime;
+    checked(read(sock, &rawtime, sizeof(time_t)));
 
-	time_t rawtime;
-	checked(read(sock, &rawtime, sizeof(time_t)));
+	// Allocating number of bytes to be received
+    unsigned char *buffer = (unsigned char *)malloc(nbytes_to_receive);
 
-	unsigned char *buffer = (unsigned char *)malloc(nbytes_to_receive);
+	// Checking malloc
+    if (buffer == NULL) {
+        fprintf(stderr, "malloc could not allocate %zd bytes",
+                nbytes_to_receive);
+        perror("");
+        exit(1);
+    }
 
-	if (buffer == NULL) {
-		fprintf(stderr, "malloc could not allocate %zd bytes", nbytes_to_receive);
-		perror("");
-		exit(1);
-	}
+    size_t total_received = 0;
+    size_t received = 1;
+	
+	// While not all bytes received 
+    while (nbytes_to_receive > 0 && received > 0) {
+        received = checked(read(sock, &buffer[total_received], nbytes_to_receive));
+        total_received += received;
+        nbytes_to_receive -= received;
+    }
 
-	size_t total_received = 0;
-	size_t received = 1;
+	// Save values
+    *now = rawtime;
+    *message = buffer;
 
-	while (nbytes_to_receive > 0 && received > 0) {
-		received = checked(read(sock, &buffer[total_received], nbytes_to_receive));
-		total_received += received;
-		nbytes_to_receive -= received;
-	}
-
-	*now = rawtime;
-	*message = buffer;
-
-	return total_received;
+    return total_received;
 }
 
-#endif // __COMMON_H
+#endif  // __COMMON_H
